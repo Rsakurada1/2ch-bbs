c22c3ed3a75a329bba37ea16246437c0c0fa070d
Rikito Sakurada <rikito82k@gmail.com>
2023-11-12 20:14:53 +0900

    変数に文字列の格納

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+if(isset($_POST["submitButton"])) {
+    $username = $_POST["username"];
+    var_dump($username);
+    $body = $_POST["body"];
+    var_dump($body);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,14 +42,14 @@
                     </div>
                 </article>
             </section>
-            <form class="formWrapper">
+            <form class="formWrapper" method="POST">
                 <div>
-                    <input type="submit" value="書き込む">
+                    <input type="submit" value="書き込む" name="submitButton">
                     <label>名前：</label>
-                    <input type="text">
+                    <input type="text" name="username">
                 </div>
                 <div>
-                    <textarea class="commentText"></textarea>
+                    <textarea class="commentText" name="body"></textarea>
                 </div>
             </form>
         </div>
